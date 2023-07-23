72eda2faed4fc572ef141a5220b29477cf754ae6
Asiri Dewthilina <asiridewthilina@Asiris-Mac-mini.local>
2023-07-23 12:02:56 +0530

    umami intergation

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <script async src="https://umamiupdate-vu9i.vercel.app/script.js" data-website-id="97bec51e-ee7d-4e6c-a8b4-050379be9ff0"></script>
       <body className={`${inter.className} background `}>
         <Navbar />
         {children}
